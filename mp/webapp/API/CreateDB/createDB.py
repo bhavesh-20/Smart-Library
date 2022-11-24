e8e35373fdd319f413e98d3e2957924686315ff9
Bhavesh <bhaveshkumar.c02@gmail.com>
2022-11-24 10:26:29 +0530

    Comment createDb.py

--- mp/webapp/API/CreateDB/createDB.py
+++ mp/webapp/API/CreateDB/createDB.py
@@ -1,20 +1,20 @@
-import MySQLdb
+# import MySQLdb
 
 
-class CreateDB:
+# class CreateDB:
 
-    def __init__(self):
-        HOST = "sql6.freemysqlhosting.net"
-        USER = "sql6580154"
-        PASSWORD = "DfAgZAZTts"
-        DATABASE = "sql6580154"
-        self.connection = MySQLdb.connect(HOST, USER, PASSWORD, DATABASE)
+#     def __init__(self):
+#         HOST = "sql6.freemysqlhosting.net"
+#         USER = "sql6580154"
+#         PASSWORD = "DfAgZAZTts"
+#         DATABASE = "sql6580154"
+#         self.connection = MySQLdb.connect(HOST, USER, PASSWORD, DATABASE)
         
-    def createTables(self):
-        with self.connection.cursor() as cursor:
-            cursor.execute(open("sql1.2.sql", "r").read())
-        self.connection.commit()
+#     def createTables(self):
+#         with self.connection.cursor() as cursor:
+#             cursor.execute(open("sql1.2.sql", "r").read())
+#         self.connection.commit()
 
-if __name__ == "__main__":
-    db = CreateDB()
-    db.createTables()
+# if __name__ == "__main__":
+#     db = CreateDB()
+#     db.createTables()
