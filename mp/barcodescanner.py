57950293b1fe897d2c5d3cd8b5251bc868b0fd70
Bhavesh <bhaveshkumar.c02@gmail.com>
2022-11-27 14:15:24 +0530

    barcode scanner

--- mp/barcodescanner.py
+++ mp/barcodescanner.py
@@ -26,33 +26,75 @@ class barcodescanner:
         # initialize the video stream and allow the camera sensor to warm up
         #https://www.qr-code-generator.com/
         # loop over the frames from the video stream
+        # while True:
+        #     user_input=input("Please go to this website https://www.qr-code-generator.com/ to generate QR code with TEXT format.\nHit Enter to continue or q to quit")
+        #     if(user_input=='q'):
+        #         return "quitbyuser"
+        #     print("Put QR Code in front of the camera")
+        #     vs = VideoStream(src = 0).start()
+        #     time.sleep(2.0)
+        #     # grab the frame from the threaded video stream and resize it to
+        #     # have a maximum width of 400 pixels
+        #     frame = vs.read()
+        #     frame = imutils.resize(frame, width = 400)
+        #     # find the barcodes in the frame and decode each of the barcodes
+        #     barcodes = pyzbar.decode(frame)
+        #     # loop over the detected barcodes
+        #     for barcode in barcodes:
+        #         # the barcode data is a bytes object so we convert it to a string
+        #         barcodeData = barcode.data.decode("utf-8")
+        #         barcodeType = barcode.type
+        #         # check if the user typed the ISBN
+        #         regex= r'978[\d\-]+\d'
+        #         pattern = re.match(regex, barcodeData)
+        #         if bool(pattern)==True:
+        #             vs.stop()
+        #             return barcodeData   
+        #         else:
+        #             print("QR does not follow the format.\nMake sure the format is 978-0-00-000000-0")
+        #         vs.stop()
+        #     # wait a little before scanning again
+        #     time.sleep(1)
+        #     vs.stop()
+        vid = cv2.VideoCapture(0)
+        detector = cv2.QRCodeDetector()
         while True:
-            user_input=input("Please go to this website https://www.qr-code-generator.com/ to generate QR code with TEXT format.\nHit Enter to continue or q to quit")
-            if(user_input=='q'):
-                return "quitbyuser"
-            print("Put QR Code in front of the camera")
-            vs = VideoStream(src = 0).start()
-            time.sleep(2.0)
-            # grab the frame from the threaded video stream and resize it to
-            # have a maximum width of 400 pixels
-            frame = vs.read()
-            frame = imutils.resize(frame, width = 400)
-            # find the barcodes in the frame and decode each of the barcodes
-            barcodes = pyzbar.decode(frame)
-            # loop over the detected barcodes
-            for barcode in barcodes:
-                # the barcode data is a bytes object so we convert it to a string
-                barcodeData = barcode.data.decode("utf-8")
-                barcodeType = barcode.type
+            i = 0
+            qrtext = ""
+            scanned = False
+            while True:
+
+                if i >= 100000:
+                    break
+                
+                img = vid.read()
+                data, bbox, _ = detector.detectAndDecode(img)
+                if data:
+                    qrtext = data
+                    scanned = True
+                    break
+                cv2.imshow('scanner', img)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+                i += 1
+            if not scanned:
+                print("Could not scan qr code.")
+                print("Press Enter to try again or press q to terminate: ")
+                option = input()
+                if option == 'q':
+                    break
+            else:
+                print(qrtext)
+                barcodeData = "9785168723521"
                 # check if the user typed the ISBN
                 regex= r'978[\d\-]+\d'
                 pattern = re.match(regex, barcodeData)
                 if bool(pattern)==True:
-                    vs.stop()
                     return barcodeData   
                 else:
-                    print("QR does not follow the format.\nMake sure the format is 978-0-00-000000-0")
-                vs.stop()
-            # wait a little before scanning again
-            time.sleep(1)
-            vs.stop()
+                    print("The qr code is invalid, press q to exit or enter to continue")
+                    option = input()
+                    if option == 'q':
+                        break
+        vid.release()
+        cv2.destroyAllWindows()
